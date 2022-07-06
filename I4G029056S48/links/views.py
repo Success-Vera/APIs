1d2e6f8bebbbcc4eeae094ef1de04f772441cad1
Success-Vera <ifysuccessvera@gmail.com>
2022-07-06 01:31:08 +0000

    added urls

--- I4G029056S48/links/views.py
+++ I4G029056S48/links/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Link
+from .serizliers import LinkSerializer
 
 # Create your views here.
+class PostCreateApi(generics.CreateAPIView):
+    queryset = Link.objects.filter(active=True)
+
+    serializer_class = LinkSerializer
+
+class PostDetailApi(generics.RetrieveAPIView):
+    queryset = Link.objects.filter(active=True)
+
+    serializer_class = LinkSerializer
+class PostUpdateApi(generics.UpdateAPIView):
+    queryset = Link.object.filter(active=True)
+
+    serializer_class = LinkSerializer
+class PostDeleteApi(generics.DestroyAPIView):
+    queryset= Link.objects.filter(active=True)
+
+    serializer_class = LinkSerializer
+
