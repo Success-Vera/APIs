b51a53817ba842f0a3c1a020a89ccb217ab7487a
Success-Vera <ifysuccessvera@gmail.com>
2022-07-06 01:01:31 +0000

    made migrations

--- I4G029056S48/links/models.py
+++ I4G029056S48/links/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Link(models.Model):
+    target_url=models.URLField(max_length=200)
+    description=models.CharField(max_length=200)
+    identifier=models.SlugField(max_length=20, blank=True, unique=True)
+    author=models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
+    created_date=models.DateTimeField()
+    active=models.BooleanField(default=True)
