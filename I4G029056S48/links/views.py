1784fd870fe613f8dc85637116acc934213cf473
Success-Vera <ifysuccessvera@gmail.com>
2022-07-06 11:46:42 +0000

    working apis

--- I4G029056S48/links/views.py
+++ I4G029056S48/links/views.py
@@ -5,23 +5,21 @@ from .serizliers import LinkSerializer
 # Create your views here.
 class PostListApi(generics.ListAPIView):
     queryset = Links.objects.filter(active=True)
-
     serializer_class = LinkSerializer
+
 class PostCreateApi(generics.CreateAPIView):
     queryset = Links.objects.filter(active=True)
-
     serializer_class = LinkSerializer
 
 class PostDetailApi(generics.RetrieveAPIView):
     queryset = Links.objects.filter(active=True)
-
     serializer_class = LinkSerializer
+
 class PostUpdateApi(generics.UpdateAPIView):
-    queryset = Links.object.filter(active=True)
-
     serializer_class = LinkSerializer
-class PostDeleteApi(generics.DestroyAPIView):
-    queryset= Links.objects.filter(active=True)
+    queryset = Links.objects.filter(active=True)
 
+class PostDeleteApi(generics.DestroyAPIView):
+    queryset= Links.objects.all()
     serializer_class = LinkSerializer
 
